cf5aaec07d4791e88240f690c662a03658b9df0f
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-05-28 11:43:28 +0400

    wasd

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Task •Ədədin kubunu göstərən proqram yazın.
 
-//using System.Reflection.Metadata.Ecma335;
+using System.Reflection.Metadata.Ecma335;
 
 //string cubeNumber = Console.ReadLine();
 
@@ -42,37 +42,75 @@ Misal: İstifadəçi 50 və 75 daxil edərsə 3cü tərəf 55ə bərabər olacaq
 
 
 
-Console.WriteLine("Enter the first angle");
-string angle1 = Console.ReadLine();
-int angle_1;
-bool case1 = int.TryParse(angle1 , out angle_1);
+//Console.WriteLine("Enter the first angle");
+//string angle1 = Console.ReadLine();
+//int angle_1;
+//bool case1 = int.TryParse(angle1, out angle_1);
 
 
-Console.WriteLine("Enter the second angle");
+//Console.WriteLine("Enter the second angle");
 
-string angle2 = Console.ReadLine();
-int angle_2;
-bool case2 = int.TryParse(angle2, out angle_2);
+//string angle2 = Console.ReadLine();
+//int angle_2;
+//bool case2 = int.TryParse(angle2, out angle_2);
 
-int triangle = angle_1 + angle_2;
+//int triangle = angle_1 + angle_2;
 
-int angle_3 = 180 - (angle_1 + angle_2);
+//int angle_3 = 180 - (angle_1 + angle_2);
 
-if (triangle > 180)
-{
-    Console.WriteLine("the sum of a triangle cannot exceed 180 degrees");
-    return;
-}
-else
-{
-    Console.WriteLine("The 3rd angle is " + angle_3);
-}
-
-
+//if (triangle > 180)
+//{
+//    Console.WriteLine("the sum of a triangle cannot exceed 180 degrees");
+//    return;
+//}
+//else
+//{
+//    Console.WriteLine("The 3rd angle is " + angle_3);
+//}
 
 
+/*
+45,67,89,23,55,88 və 93 rəqəmindən ibarət massivimiz var. 
+İstifadəçidən hər hansı bir rəqəm yazmasını tələb edərək həmin rəqəmin massivdə olub olmaması yoxlayın. 
+Əgər rəqəm ədəd varsa “xx rəqəmi massivdə var” və ya “xx rəqəmi massivdə yoxdur” şəklində ekrana yazı çıxardın
+ */
 
 
+//int[] arrA = { 45, 67, 89, 23, 55, 88, 93 };
+
+
+//int number = Convert.ToInt32(Console.ReadLine());
+
+
+//for (int i = 0; i < arrA.Length; i++)
+//{
+
+//    if (number == arrA[i])
+//    {
+//        Console.WriteLine(number + " is in the array");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine(number + " is not in the array");
+//        //return;
+//        //break;
+//    }
+
+//}
+
+
+
+
+
+//•(8x+45):25-9 sadə düsturuna uyğun funksiya düzəldin. Funksiya x-ı qəbul edərək hesablama aparsın.
+
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+int equation = (8 * x + 45) / 25 - 9;
+
+Console.WriteLine(equation);
 
 
 
